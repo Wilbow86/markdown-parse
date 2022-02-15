72ae3320c79ed50762955f30bdbcb5dc54093c92
Wilbow86 <97714424+Wilbow86@users.noreply.github.com>
2022-02-14 22:54:06 -0800

    Update and rename OldMarkdownParseTest.java to CLinMarkdownParseTest.java

--- CLinMarkdownParseTest.java
+++ CLinMarkdownParseTest.java
@@ -7,7 +7,7 @@ import java.nio.file.Path;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class OldMarkdownParseTest {
+public class CLinMarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
